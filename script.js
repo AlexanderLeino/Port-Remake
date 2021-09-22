713de9302aabf027516b59b8fd1f612641b874f5
Alexander Leino <Alexander.Leino@yahoo.com>
2021-09-22 02:11:14 -0400

    refactored most of code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let submitBtn = document.getElementById('submitBtn')
 submitBtn.addEventListener('click', userSubmitedForm)
-let formContainer = document.getElementById('formContainer')
+let submitTextHolder = document.getElementById('submitTextHolder')
 
 
 let formEl = document.getElementById('contactMe')
@@ -10,7 +10,9 @@ function userSubmitedForm(e){
     console.log('remove')
   formEl.remove()
   let submitResponse = document.createElement('h1')
-  submitResponse.innerText = "Thank you for your Submission I look forward to working with you!"
-  formContainer.appendChild(submitResponse)
+  submitResponse.innerText = "Thank you for your Submission I look forward to working with you! Feel free to connect with my on Facebook or Linkedin"
+  submitResponse.style.marginTop = "20px"
+  submitResponse.style.fontFamily = "var(--bs-body-font-family"
+  submitTextHolder.appendChild(submitResponse)
 
 }
